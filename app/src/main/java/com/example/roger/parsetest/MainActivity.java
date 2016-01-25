b76b9b5973bebfe305770e3e33394b02b810668d
Roger Wang <roger35972134@gmail.com>
2016-01-26 00:10:59 +0800

    2016/1/26
    add duplicate ID check

--- app/src/main/java/com/example/roger/parsetest/MainActivity.java
+++ app/src/main/java/com/example/roger/parsetest/MainActivity.java
@@ -1,73 +1,119 @@
 package com.example.roger.parsetest;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import com.parse.FindCallback;
-import com.parse.Parse;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
+public class MainActivity extends Activity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-import java.text.ParseException;
-import java.util.List;
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
 
-public class MainActivity extends AppCompatActivity {
-    String Id, password;
-    @Bind(R.id.edtId)
-    EditText edt_Id;
-    @Bind(R.id.edtpassword)
-    EditText edt_password;
-
-    @OnClick(R.id.signup)
-    void onSignUpClick() {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.login)
-    void onClick() {
-        Id = edt_Id.getText().toString();
-        password = edt_password.getText().toString();
-        ParseQuery query = ParseQuery.getQuery("UserData");
-        query.whereEqualTo("Id", Id).whereEqualTo("password", password);
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, com.parse.ParseException e) {
-                if (e == null) {
-                    Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_LONG).show();
-                } else {
-                    //Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
-                    Toast.makeText(MainActivity.this, "Account error", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-        edt_Id.setText("");
-        edt_password.setText("");
-    }
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initParse();
-        ButterKnife.bind(this);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-    public void initParse() {
-        try {
-            Parse.initialize(this, "BAMBLehMHOD8NeRcVAaRMbRVgFEnfAuAmJm4QKpR", "uS2MUeHZHzya4rGTC3UlG0TI0gSEClxuKbnHFC60");
-            ParseInstallation.getCurrentInstallation().saveInBackground();
-        } catch (Exception e) {
-            //do nothing
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
         }
 
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
     }
 
 }
--- app/src/main/java/com/example/roger/parsetest/SignUpActivity.java
+++ app/src/main/java/com/example/roger/parsetest/SignUpActivity.java
@@ -2,11 +2,21 @@ package com.example.roger.parsetest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.nispok.snackbar.SnackbarManager;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,43 +24,79 @@ import butterknife.OnClick;
 
 
 public class SignUpActivity extends Activity {
+    boolean flag = true;
     @Bind(R.id.signID)
     EditText signId;
     @Bind(R.id.signPW)
     EditText signPw;
     @Bind(R.id.signEmail)
     EditText signEmail;
-    @OnClick(R.id.imgCheck) void onClick(){
-        String id=signId.getText().toString();
-        String password=signPw.getText().toString();
-        String email=signEmail.getText().toString();
-        if(!id.isEmpty()&&!password.isEmpty()&&!email.isEmpty())
-        {
-            updateData(id, password, email);
-            Toast.makeText(this,"Sign up success",Toast.LENGTH_LONG);
-            Intent intent=new Intent(this,MainActivity.class);
-            startActivity(intent);
-        }else{
-            Toast.makeText(this,"You should fill all the slots",Toast.LENGTH_LONG).show();
+
+    @OnClick(R.id.imgCheck)
+    void onClick() {
+        final String id = signId.getText().toString();
+        final String password = signPw.getText().toString();
+        final String email = signEmail.getText().toString();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (flag) {
+                    updateData(id, password, email);
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        Check(id, password, email);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        thread.run();
     }
-    @OnClick(R.id.returnpage) void onReturnClick(){
-        Intent intent=new Intent(this,MainActivity.class);
+
+    @OnClick(R.id.returnpage)
+    void onReturnClick(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
     }
-    public void updateData(String ID, String PW,String Email) {
+
+    public void Check(String Id, String Password, String Email) {
+        flag = true;
+        if (Id.isEmpty() && Password.isEmpty() && Email.isEmpty()) {
+            Toast.makeText(this, "You should fill all the slots", Toast.LENGTH_LONG).show();
+            flag = false;
+        }
+        ParseQuery query = ParseQuery.getQuery("UserData");
+        query.whereEqualTo("Id", Id);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (objects.size() != 0) {
+                    Toast.makeText(SignUpActivity.this, "ID have been used by another user", Toast.LENGTH_LONG).show();
+                    flag = false;
+                } else if (e != null) {
+                    Toast.makeText(SignUpActivity.this, "Server error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+    public void updateData(String ID, String PW, String Email) {
         ParseObject userdata = new ParseObject("UserData");
         userdata.put("Id", ID);
         userdata.put("password", PW);
-        userdata.put("email",Email);
+        userdata.put("email", Email);
         userdata.saveInBackground();
+        Toast.makeText(SignUpActivity.this, "Sign up success", Toast.LENGTH_LONG).show();
+
     }
 }
 
